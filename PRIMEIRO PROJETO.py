e52947870ab8d2c2f3fc35fe26acb72324d9ded4
Mark Alves <109834671+Markalvs@users.noreply.github.com>
2026-07-15 03:14:36 -0300

    Delete PRIMEIRO PROJETO.py

--- PRIMEIRO PROJETO.py
+++
@@ -1,9 +0,0 @@
-numero_1 = float(input('Digite um número: '))
-numero_2 = float(input('Digite outro número: '))
-
-resultado = float(numero_1) * float (numero_2)
-
-print (f'O resultado é {resultado}')
-
-
-
