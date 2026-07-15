c432af0a8ea35a8cbed13308bfe251b06e9ad447
Mark Alves <markvinicius180@gmail.com>
2026-07-15 02:59:25 -0300

    Primeiro comit

---
+++ PRIMEIRO PROJETO.py
@@ -0,0 +1,9 @@
+numero_1 = float(input('Digite um número: '))
+numero_2 = float(input('Digite outro número: '))
+
+resultado = float(numero_1) * float (numero_2)
+
+print (f'O resultado é {resultado}')
+
+
+
