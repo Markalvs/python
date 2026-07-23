e068d9c1b9593df201a657bd881bcff61f5c8271
Mark Alves <markvinicius180@gmail.com>
2026-07-23 02:23:06 -0300

    Adiciona aula 34 - lógica de decisão

---
+++ aula 34.py
@@ -0,0 +1,11 @@
+#lógica de decisão
+
+
+entrada = input ('Você quer "entrar " ou "sair"?')
+
+if entrada =='entrar':
+    print ('Bem-vindo!')
+elif entrada == 'sair':
+    print ('Até logo!')
+else:
+    print ('Opção inválida!')
