f687883ab42858a258ccd6a8a18a2004b3166a66
Mark Alves <markvinicius180@gmail.com>
2026-07-25 02:29:47 -0300

    Cálculadora

--- aula 34.py
+++ aula 34.py
@@ -1,11 +1,10 @@
 #lógica de decisão
 
-
 entrada = input ('Você quer "entrar " ou "sair"?')
 
 if entrada =='entrar':
     print ('Bem-vindo!')
 elif entrada == 'sair':
-    print ('Até logo!')
+    print ('Até logo.')
 else:
     print ('Opção inválida!')
