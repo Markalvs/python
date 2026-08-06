51200550d56695e92f95e230d5806383a35c3fd1
Mark Alves <markvinicius180@gmail.com>
2026-08-06 19:42:10 -0300

    Atualizando exercícios

---
+++ fundamentos/try.py
@@ -0,0 +1,9 @@
+"""
+Introdução ao try/except
+try -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+"""
+
+numero = float(input("Insirá um número e seu valor será dobrado: "))
+resultado = numero * 2
+print (f'O dobro de {numero} é {resultado}')
