2f1239fd176464bb9becd788f6fbda565fbccf8c
Mark Alves <markvinicius180@gmail.com>
2026-08-08 18:06:28 -0300

    Atualizando exercícios

--- fundamentos/exercicio32.1.py
+++ fundamentos/exercicio32.1.py
@@ -7,9 +7,9 @@ try:
     numero = int(input("Digite um número inteiro: ")) 
 
     if numero % 2 == 0:
-        print ("Esse número é par")
+        print (f"O número {numero} é par")
     else:
-        print ("Esse número é impar")
+        print (f"O número {numero} é impar")
 except ValueError:
     print ("Isso não é um número inteiro")
 
