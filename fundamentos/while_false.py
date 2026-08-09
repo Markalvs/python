f4c1aa7ca692e73ca704185da016595cbf326882
Mark Alves <markvinicius180@gmail.com>
2026-08-09 01:00:10 -0300

    Testando restrição do contator utilizando while

---
+++ fundamentos/while_false.py
@@ -0,0 +1,10 @@
+
+
+
+contador = 0 #atribuindo o primeiro valor ao contador
+
+while contador < 10:
+    contador = contador + 1 #reatribuindo um valor ao contador até atingir a condição
+    print (contador)
+
+print ("Acabou")
