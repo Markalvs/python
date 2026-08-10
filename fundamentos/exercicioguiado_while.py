a5d94d46ce620dfaa1efd6aac48e7344452a7345
Mark Alves <markvinicius180@gmail.com>
2026-08-10 18:40:58 -0300

    Aprendendo a criar estruturas com While

---
+++ fundamentos/exercicioguiado_while.py
@@ -0,0 +1,13 @@
+"""
+Iterando strings com while
+"""
+ #      0123456789 indicis da string
+nome = 'Mark Alves' # String são iteráveis
+#             321
+tamanho_nome = len(nome)
+
+indice = 0
+while indice < len(nome):
+    print(nome[indice])
+    indice += 1
+
