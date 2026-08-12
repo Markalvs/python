f1592bb9462dad2aaa7853c165ad71ba7b427a0e
Mark Alves <markvinicius180@gmail.com>
2026-08-12 18:50:37 -0300

    Entendendo a diferença da lógica de while e for

---
+++ fundamentos/for_in.py
@@ -0,0 +1,22 @@
+"""
+texto = 'Python s2'
+
+i = 0
+tamanho_string = len(texto)
+
+while i < tamanho_string:
+    print(texto[i], i)
+
+    i += 1
+"""
+
+senha_salva = '123456'
+senha_digitada = ''
+repeticoes = 0
+
+while senha_salva!= senha_digitada:
+    senha_digitada = input (f'Sua senha({repeticoes}x): ')
+
+    repeticoes += 1
+
+print ('Aquele laço acima pode ter repetições infinitas')
