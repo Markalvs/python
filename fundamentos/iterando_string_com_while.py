0385f69d6a35d576b46c89a5bb7b6c29ef1cba89
Mark Alves <markvinicius180@gmail.com>
2026-08-12 01:00:28 -0300

    praticando

--- fundamentos/iterando_string_com_while.py
+++ fundamentos/iterando_string_com_while.py
@@ -1,7 +1,4 @@
-frase = 'O python é uma linguagem de programação, multiparadgima, python foi criado por guido van rossum.'
-
-print(frase.lower().count('python'))
-
+frase = 'aaaooo'
 
 i = 0
 qtd_apareceu_mais_vezes = 0
@@ -9,11 +6,21 @@ letra_apareceu_mais_vezes = ''
 
 while i < len(frase):
     letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
     qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
 
     if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
         qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
         letra_apareceu_mais_vezes = letra_atual
 
-    print(letra_atual, qtd_apareceu_mais_vezes_atual)
     i += 1
+
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_apareceu_mais_vezes}" que apareceu '
+    f'{qtd_apareceu_mais_vezes}x'
+)
