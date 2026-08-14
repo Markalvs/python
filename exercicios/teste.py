160b20c314b5b51c31d873f02d9b113ab4947c8a
Mark Alves <markvinicius180@gmail.com>
2026-08-14 16:02:57 -0300

    Fazendo pequeno teste no código

---
+++ exercicios/teste.py
@@ -0,0 +1,9 @@
+conhecimento = 1
+
+while True:
+    print(f'Estudar: {conhecimento}')
+    print(f'Praticar: {conhecimento}')
+    print(f'Errar: {conhecimento}')
+    print(f'Corrigir: {conhecimento}')
+    
+    conhecimento += 1
